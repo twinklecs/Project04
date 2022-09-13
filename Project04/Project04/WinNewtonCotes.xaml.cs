7a53949d3b81ee2591e3dedd057430fcfe0f4015
Twinkle <dim.iwanencko2014@gmail.com>
2022-09-13 23:59:10 +0300

    add project

---
+++ Project04/Project04/WinNewtonCotes.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Project04
+{
+    /// <summary>
+    /// Логика взаимодействия для WinNewtonCotes.xaml
+    /// </summary>
+    public partial class WinNewtonCotes : Window
+    {
+        public WinNewtonCotes()
+        {
+            InitializeComponent();
+        }
+
+        private void S_Closed(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        private double Steps(double a, double b, double n)
+        {
+            return (b - a) / n;
+        }
+
+
+    }
+}
